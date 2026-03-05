583cb8595f61e7b829d832761e7b5c1facc3787c
Aarti Rani <aartirani9841@gmail.com>
2026-03-05 15:07:43 +0530

    add sslmode

--- assetmanagement_1/settings.py
+++ assetmanagement_1/settings.py
@@ -78,14 +78,17 @@ WSGI_APPLICATION = 'assetmanagement_1.wsgi.application'
 # https://docs.djangoproject.com/en/6.0/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.postgresql',
-        'NAME': os.getenv("DB_NAME"),
-        'USER': os.getenv("DB_USER"),
-        'PASSWORD': os.getenv("DB_PASSWORD"),
-        'HOST': os.getenv("DB_HOST"),
-        'PORT': os.getenv("DB_PORT"),
-    }
+ 'default': {
+   'ENGINE': 'django.db.backends.postgresql',
+   'NAME': os.environ.get("DB_NAME"),
+   'USER': os.environ.get("DB_USER"),
+   'PASSWORD': os.environ.get("DB_PASSWORD"),
+   'HOST': os.environ.get("DB_HOST"),
+   'PORT': os.environ.get("DB_PORT"),
+   'OPTIONS': {
+       'sslmode': 'require'
+   }
+ }
 }
 
 CSRF_TRUSTED_ORIGINS = [
